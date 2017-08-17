3804acda40476f7084a37e215cd5f8b40e92337e
Drew Mercer <japanexpress@gmail.com>
2017-08-17 13:01:35 -0400

    Adding loop to setValue function

--- js/main.js
+++ js/main.js
@@ -10,11 +10,18 @@ var display = document.getElementById("display");
 var equalTo = 0;
 
 function setValue(number) {
+    if(equalTo === 1) {
+        clearButton();
+    }
     if(flag === 0) {
         num1 += number;
+        display.innerHTML += number;
     }
     else {
-
+        num2 += number;
+        display.innerHTML += number;
     }
-
+    if(num1.length > 10 || num2.length > 10) {
+        display.innerHTML = 'Maximum limit of digits reached';
+    }
 }

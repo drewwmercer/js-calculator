6a8f9b7c0b8a0696ec70ff8d277d3ea582711f46
Drew Mercer <japanexpress@gmail.com>
2017-08-17 13:38:00 -0400

    Formatted and prevent showing multiple operators

--- js/main.js
+++ js/main.js
@@ -10,41 +10,40 @@ var display = document.getElementById("display");
 var equalTo = 0;
 
 function setValue(number) {
-    if(equalTo === 1) {
-        clearButton();
-    }
-    if(flag === 0) {
-        num1 += number;
-        display.innerHTML += number;
-    }
-    else {
-        num2 += number;
-        display.innerHTML += number;
-    }
-    if(num1.length > 10 || num2.length > 10) {
-        display.innerHTML = 'Maximum limit of digits reached';
-    }
+  if (equalTo === 1) {
+    clearButton();
+  }
+  if (flag === 0) {
+    num1 += number;
+    display.innerHTML += number;
+  } else {
+    num2 += number;
+    display.innerHTML += number;
+  }
+  if (num1.length > 10 || num2.length > 10) {
+    display.innerHTML = "Maximum limit of digits reached";
+  }
 }
 
 function oppClick(numericCode) {
-    operator = numericCode;
-    var oppString = "";
-    flag = 1;
-        if(operator === 4) {
-            display.innerHTML += "/";
-            oppString = "/";
-        }
-        else if(operator === 3) {
-            display.innerHTML += "*";
-            oppString = "*";
-        }
-        else if(operator === 2) {
-            display.innerHTML += "-";
-            oppString = "-";
-        }
-        else {
-            display.innerHTML += "+";
-            oppString = "+";
-        }
+  operator = numericCode;
+  var oppString = "";
+  flag = 1;
+  if (operator === 4) {
+    display.innerHTML += "/";
+    oppString = "/";
+  } else if (operator === 3) {
+    display.innerHTML += "*";
+    oppString = "*";
+  } else if (operator === 2) {
+    display.innerHTML += "-";
+    oppString = "-";
+  } else {
+    display.innerHTML += "+";
+    oppString = "+";
+  }
 
+  if (flag === 1) {
+    display.innerHTML = num1 + oppString;
+  }
 }
